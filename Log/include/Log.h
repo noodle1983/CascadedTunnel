ef4f24b10a8c09e9a00c4910847434d34ac80556
LuDong <noodle1983@126.com>
2020-06-23 14:40:05 +0800

    log add file line

--- Log/include/Log.h
+++ Log/include/Log.h
@@ -2,44 +2,70 @@
 #define LOG_H
 
 #include "Logger.h"
+#include <boost/thread/thread.hpp>
 
 //-----------------------------------------------------------------------------
 //trafficLogger
 #define LOG_TRACE(msg) \
-BOOST_LOG_SEV(g_logger->lg, trace)<<msg
+BOOST_LOG_SEV(g_logger->lg, trace) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define LOG_DEBUG(msg) \
-BOOST_LOG_SEV(g_logger->lg, debug)<<msg
+BOOST_LOG_SEV(g_logger->lg, debug)\
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define LOG_INFO(msg) \
-BOOST_LOG_SEV(g_logger->lg, info)<<msg
+BOOST_LOG_SEV(g_logger->lg, info) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define LOG_WARN(msg) \
-BOOST_LOG_SEV(g_logger->lg, warning)<<msg
+BOOST_LOG_SEV(g_logger->lg, warning) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define LOG_ERROR(msg) \
-BOOST_LOG_SEV(g_logger->lg, error)<<msg
+BOOST_LOG_SEV(g_logger->lg, error) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define LOG_FATAL(msg) \
-BOOST_LOG_SEV(g_logger->lg, fatal)<<msg
+BOOST_LOG_SEV(g_logger->lg, fatal) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 //-----------------------------------------------------------------------------
 //configLogger
 #define CFG_TRACE(msg) \
-BOOST_LOG_SEV(g_cfg_logger->lg, trace)<<msg
+BOOST_LOG_SEV(g_cfg_logger->lg, trace) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define CFG_DEBUG(msg) \
-BOOST_LOG_SEV(g_cfg_logger->lg, debug)<<msg
+BOOST_LOG_SEV(g_cfg_logger->lg, debug) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define CFG_INFO(msg) \
-BOOST_LOG_SEV(g_cfg_logger->lg, info)<<msg
+BOOST_LOG_SEV(g_cfg_logger->lg, info) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define CFG_WARN(msg) \
-BOOST_LOG_SEV(g_cfg_logger->lg, warning)<<msg
+BOOST_LOG_SEV(g_cfg_logger->lg, warning) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define CFG_ERROR(msg) \
-BOOST_LOG_SEV(g_cfg_logger->lg, error)<<msg
+BOOST_LOG_SEV(g_cfg_logger->lg, error) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
 
 #define CFG_FATAL(msg) \
-BOOST_LOG_SEV(g_cfg_logger->lg, fatal)<<msg
+BOOST_LOG_SEV(g_cfg_logger->lg, fatal) \
+    << "(" <<  __FILE__ << ":" << __LINE__ << ") "  \
+    << msg
+
 #endif /* LOG_H */
