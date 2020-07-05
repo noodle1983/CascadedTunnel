2ff368a7fea663d2e8edc89eea8f752fb3b0aa29
LuDong <noodle1983@126.com>
2020-07-05 15:03:28 +0800

    app on Server.cpp

--- TunnelServer/UnitTest/Server.cpp
+++ TunnelServer/UnitTest/Server.cpp
@@ -6,6 +6,7 @@
 #include "ProcessorSensor.h"
 #include "Log.h"
 #include "ConfigCenter.h"
+#include "App.h"
 
 #include <event.h>
 #include <event2/thread.h>
@@ -15,41 +16,10 @@
 using namespace std;
 using namespace nd;
 
-static int closed = false;
-static mutex closedMutexM;
-static condition_variable closedCondM;
-void sig_stop(int sig)
+int main(int argc, char *argv[])
 {
-    LOG_DEBUG("receive signal " << sig << ". stopping...");
-    lock_guard<mutex> lock(closedMutexM);
-    closed = true;
-    closedCondM.notify_one();
-}
+    g_app->parseAndInit(argc, argv);
 
-int main()
-{
-    bool runInBackground = g_cfg->get("process.background", 1);
-    if (runInBackground)
-    {
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            printf("failed to fork!\n");
-            return -1;
-        }
-        else if (pid > 0)
-        {
-            printf("server start with pid %d.\n", pid);
-            return 0;
-        }
-        setsid();
-    }
-
-    signal(SIGPIPE, SIG_IGN);
-    signal(SIGALRM, SIG_IGN);
-    signal(SIGTERM, sig_stop);
-    signal(SIGINT, sig_stop);
-    evthread_use_pthreads();
     TunnelServerProtocol serverProtocol(g_net_processor);
     TunnelProxyProtocol proxyProtocol(g_net_processor, &serverProtocol);
     serverProtocol.setProxyProtocol(&proxyProtocol);
@@ -58,12 +28,10 @@ int main()
     innerServer.start();
     proxyServer.start();
 
-    raise(SIGINT);
-    unique_lock<mutex> lock(closedMutexM);
-    while(!closed)
-    {
-        closedCondM.wait(lock);
-    }
+    g_app->wait();
+    g_reactor->stop();
+    proxyServer.stop();
+    innerServer.stop();
 
     LOG_DEBUG("TunnelServer stopped.");
     return 0;
